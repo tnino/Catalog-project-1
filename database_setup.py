8d75db3d0d805b881e2217b70381ef0bcab78849
tnino <30389051+tnino@users.noreply.github.com>
2018-06-25 21:05:59 -0500

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 import flask
 import sqlalchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
